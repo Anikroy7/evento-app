a8d8727352090695801b73835708efe6eb4c71a5
Anikroy7 <roy645800@gmail.com>
2023-02-25 19:48:48 +0600

    initial the project includes material ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+
+function App() {
+  return (
+    <div>
+      Hello evento
+      <Button variant="contained"> Evento</Button>
+    </div>
+  )
+}
+
+export default App
